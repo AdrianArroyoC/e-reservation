c4c76c1914a66d5bb993bb5706a57af970a85117
Adrian Arroyo Ceja <adrianarroyoceja@users.noreply.github.com>
2019-01-31 17:49:35 -0600

    Decimo segundo commit
    Documentación de los servicios REST con Swagger

--- src/main/java/com/adrianarroyoc/ereservation/views/resources/ClientResource.java
+++ src/main/java/com/adrianarroyoc/ereservation/views/resources/ClientResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -16,6 +17,11 @@ import com.adrianarroyoc.ereservation.controllers.services.ClientService;
 import com.adrianarroyoc.ereservation.models.Client;
 import com.adrianarroyoc.ereservation.views.resources.vo.ClientVO;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
 /**
  * Class that represents the client web service
  * @author adrianarroyoc
@@ -24,6 +30,7 @@ import com.adrianarroyoc.ereservation.views.resources.vo.ClientVO;
 
 @RestController
 @RequestMapping("/api/client")
+@Api(tags = "client")
 public class ClientResource {
 
 	private final ClientService clientService;
@@ -33,6 +40,11 @@ public class ClientResource {
 	}
 	
 	@PostMapping
+	@ApiOperation(value = "Create Client", notes = "Service for the creation operation of a new client")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "Client created"), 
+			@ApiResponse(code = 400, message = "Invalid request")
+			})
 	public ResponseEntity<Client> createClient(@RequestBody ClientVO clientVo) {
 		Client client = new Client();
 		client.setNameCli(clientVo.getNameCli());
@@ -45,6 +57,11 @@ public class ClientResource {
 	}
 	
 	@PutMapping("/{identification}")
+	@ApiOperation(value = "Update Client", notes = "Service for the update operation of a client")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "Client updated"), 
+			@ApiResponse(code = 400, message = "Client not found")
+			})
 	public ResponseEntity<Client> updateClient(@PathVariable("identification") String identification, ClientVO clientVo) {
 		Client client = this.clientService.findByIdentification(identification);
 		if (client == null) {
@@ -61,6 +78,11 @@ public class ClientResource {
 	}
 
 	@DeleteMapping("/{identification}")
+	@ApiOperation(value = "Delete Client", notes = "Service for the delete operation of a client")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "Client deleted"), 
+			@ApiResponse(code = 400, message = "Client not found")
+			})
 	public void removeClient(@PathVariable("identification") String identification) {
 		Client client = this.clientService.findByIdentification(identification);
 		if (client != null) {
@@ -68,6 +90,12 @@ public class ClientResource {
 		}
 	}
 	
+	@GetMapping
+	@ApiOperation(value = "List Clients", notes = "Service for the find operation of clients")
+	@ApiResponses(value = {
+			@ApiResponse(code = 201, message = "Clients found"), 
+			@ApiResponse(code = 400, message = "Clients not found")
+			})
 	public ResponseEntity<List<Client>> findAll() {
 		return ResponseEntity.ok(this.clientService.findAll());
 	}

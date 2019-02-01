01defc75ef798bc1e9c3a9566b6e9334ceff7438
Adrian Arroyo Ceja <adrianarroyoceja@users.noreply.github.com>
2019-02-01 12:20:44 -0600

    Decimo cuarto commit
    Personalizar la página de Login

--- src/main/java/com/adrianarroyoc/ereservation/utils/BasicConfiguration.java
+++ src/main/java/com/adrianarroyoc/ereservation/utils/BasicConfiguration.java
@@ -1,9 +1,13 @@
 package com.adrianarroyoc.ereservation.utils;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * Class to manage the security
@@ -16,13 +20,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class BasicConfiguration extends WebSecurityConfigurerAdapter {
 
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.anyRequest()
-			.authenticated()
-			.and()
-			.formLogin()
-			.and()
-			.httpBasic();
+		http.csrf().disable()
+		.authorizeRequests().anyRequest().authenticated()
+		.and()
+		.formLogin().loginPage("/app/login").permitAll()
+			.failureUrl("/app/login?error=true")
+			.defaultSuccessUrl("/app/home");
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		auth.inMemoryAuthentication().withUser("adrian").password(encoder.encode("platzi")).roles("USER");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
 	}
 	
 }
